e52c2c79d0bf8f7be91626285661d13788f65a49
N Young <60322086+nai-young@users.noreply.github.com>
2020-07-27 21:53:55 +0200

    Crear y arrancar servidor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Curso Node
+
+Crear y arrancar servidor
--- ejemplos/servidor_basico/index.js
+++ ejemplos/servidor_basico/index.js
@@ -1,1 +1,14 @@
 // cargar libreria http
+const http = require('http')
+
+// definir un servidor y darle un listener
+const server = http.createServer(function (request, response) {
+  response.writeHead(200, {
+    'Content-type': 'text/html'
+  })
+  response.end('Wake up, <b>Neo...</b>')
+})
+
+// arrancar el servidor
+server.listen(1338, '127.0.0.1')
+console.log('Server loaded on http://127.0.0.1:1338')
