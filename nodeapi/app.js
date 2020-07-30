a8ddf5d0b60eacaa8c96c20db1056430d985eb11
N Young <60322086+nai-young@users.noreply.github.com>
2020-07-30 22:34:57 +0200

    Peticiones y validación

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,46 +1,51 @@
-/* eslint-disable semi */
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var app = express();
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // ante cada peticion se ejecutan los siguientes middlewares
 app.use(function (req, res, next) {
   console.log('soy un middleware')
   next()
 })
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 
 // si llamamos a next pasandole algo
 app.use(function(err, req, res, next) {
+  if (err.array) { // error de validacion
+    err.status = 422
+    const errInfo = err.array({ onlyFirstError: true })[0]
+    err.message = `El parámetro ${errInfo.param} ${errInfo.msg}`
+  }
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
--- nodeapi/routes/index.js
+++ nodeapi/routes/index.js
@@ -1,10 +1,55 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+// const expressValidator = require('express-validator')
+// const query = expressValidator.query
+// const validationResult = expressValidator.validationResult
+const { query, validationResult } = require('express-validator')
 
 /* GET home page. */
 // esto es un middleware
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+// Peticion en la ruta
+router.get('/paramenruta/:etiqueta', (req, res, next) => {
+  console.log(req.params)
+  res.send('ok')
+})
+
+// Este no se ejecutaria, porque ya lee en la ruta antes
+// Responde con localhost:3000/paramenruta
+router.get('/paramenruta/:tag?', (req, res, next) => {
+  console.log(req.params)
+  res.send('paramenruta ruta')
+})
+
+// http://localhost:3000/parametros/rojo/piso/2/puerta/C
+router.get('/parametros/:id([0-9]+)/piso/:piso/puerta/:puerta', (req, res, next) => {
+  console.log(req.params)
+  res.send('ok')
+})
+
+// Peticion en Query String
+// http://localhost:3000/enquerystring?color=rojo&talla=L&almacen=madrid
+router.get('/enquerystring',
+  query('color').isAlpha().withMessage('debería ser texto'),
+  (req, res, next) => {
+    validationResult(req).throw() // Si no valida, lanza throw
+    console.log(req.query)
+    res.send('ok en query string')
+  }
+)
+
+// Petición en el body
+// Segun el protocolo no se hace peticion get en el body
+router.post('/enelbody', (req, res, next) => {
+  console.log(req.body)
+  if (req.body.color !== 'rojo') {
+    next(new Error('color no admitido: rojo'))
+    return
+  }
+  res.send('ok en el body')
+})
+
+module.exports = router
