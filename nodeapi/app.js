99d25c3b0da1a8b459f95c4219474ddc10730012
N Young <60322086+nai-young@users.noreply.github.com>
2020-07-31 00:19:13 +0200

    Cambiar motor de vistas

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -9,7 +9,8 @@ var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('view engine', 'html')
+app.engine('html', require('ejs').__express)
 
 // Establecemos la variable title para todas las vistas
 app.locals.title = 'Express'
